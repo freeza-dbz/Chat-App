5edf69aa628dc155b0c2c1164c2348bff66b2cee
Anmol Singh <anmol2006.chauhan@gmail.com>
2026-03-22 16:29:55 +0530

    Add middleware for security and logging; include cookie-parser, cors, helmet, and morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 
 const app = express();
 dotenv.config()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    methods: "GET, POST, PUT, PATCH, DELETE, HEAD",
+    credentials: true
+}))
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static("cache"));
+app.use(morgan("dev"));
+app.use(helmet());
+
 
 export { app }
